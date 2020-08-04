be00cd3a3ea9c96dfd7b374e44840f132fb979f2
bozhidarbozhilov <bozhidarbozhilov@gmail.com>
2020-08-04 07:55:43 +0300

    Initial commit. Project skeleton.

---
+++ src/main/java/com/bozhilov/mysolarplant/MySolarPlantApplication.java
@@ -0,0 +1,13 @@
+package com.bozhilov.mysolarplant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySolarPlantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySolarPlantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bozhilov/mysolarplant/MySolarPlantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bozhilov.mysolarplant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySolarPlantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
